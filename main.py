c4ccebd1cd38e735817f05cebab082fa9a64539e
carolstoffel <carol_stoffel@hotmail.com>
2024-10-20 09:28:23 -0300

    debug 2 messages response

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import discord
 import logging
 import requests
@@ -12,6 +11,8 @@ logging.basicConfig(level=logging.INFO)
 discord_token = os.getenv("DISCORD_TOKEN")
 cat_fact_url = os.getenv("CAT_FACT_URL")
 conversion_currency_url = os.getenv("CONVERSION_CURR_URL")
+
+
 # Create a Discord client instance
 intents = discord.Intents.default()
 intents.message_content = True
@@ -27,22 +28,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$cat'):
-        response = requests.get(cat_fact_url)
-        assert response.status_code == 200
-        assert "fact" in response.json()
-        
-        logging.info(f"Sending cat fact {response.json()['fact']}")
-
-        await message.channel.send(response.json()['fact'])
-
+    # Ensure that the message is not handled more than once
     if message.content.startswith('$usd'):
+        # Log the message content to check if it's being processed more than once
+        logging.info(f"Received message: {message.content}")
+
+        # Only send one response
         response = requests.get(f"{conversion_currency_url}usd.json")
-        
         logging.info(f"Sending conversion from USD to CLP {response.json()['usd']['clp']}")
 
         await message.channel.send(f"1 USD = {response.json()['usd']['clp']} CLP")
-        
+
+    elif message.content.startswith('$cat'):
+        response = requests.get(cat_fact_url)
+        assert response.status_code == 200
+        assert "fact" in response.json()
+
+        logging.info(f"Sending cat fact {response.json()['fact']}")
+        await message.channel.send(response.json()['fact'])
+
 webserver.keep_alive()
 client.run(discord_token)
-
